7e97f7732dcb6f66eb7c5cfd765157d3faa8aee2
karol <karol.kot.2007@gmail.com>
2024-10-01 11:36:05 +0200

    dddd

--- Issue2/queries/AlbumInfoQuery.sql
+++ Issue2/queries/AlbumInfoQuery.sql
@@ -1,7 +1,16 @@
 SELECT 
-artists.name,
-albums.Title,
-artists.ArtistId,albums.AlbumId,tracks.TrackId,tracks.name
-FROM artists
-JOIN albums on artists.ArtistId=albums.ArtistId , tracks on  albums.AlbumId=tracks.TrackId
-ORDER by artists.ArtistId,albums.AlbumId,tracks.TrackId
+  artists.name, 
+  albums.Title, 
+  artists.ArtistId, 
+  albums.AlbumId, 
+  tracks.TrackId, 
+  tracks.name 
+FROM 
+  artists 
+  JOIN albums on artists.ArtistId = albums.ArtistId, 
+  tracks on albums.AlbumId = tracks.AlbumId 
+ORDER by 
+  artists.ArtistId, 
+  albums.AlbumId, 
+  tracks.TrackId
+
--- Issue2/queries/AlbumView.sql
+++ Issue2/queries/AlbumView.sql
@@ -1,1 +1,17 @@
-SELECT *from tracks
+CREATE VIEW AlbumTracks1 AS 
+SELECT 
+  artists.name, 
+  albums.Title, 
+  artists.ArtistId, 
+  albums.AlbumId, 
+  tracks.TrackId, 
+  tracks.name 
+FROM 
+  artists 
+  JOIN albums on artists.ArtistId = albums.ArtistId 
+  JOIN tracks on albums.AlbumId = tracks.AlbumId 
+ORDER by 
+  artists.ArtistId, 
+  albums.AlbumId, 
+  tracks.TrackId
+
