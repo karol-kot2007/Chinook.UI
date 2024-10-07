534ccd11181b20d8070a23802e2e7f1dfb9ff8b5
karol <karol.kot.2007@gmail.com>
2024-10-07 09:32:53 +0200

    new dll added

--- Issue4/BookLibrary.Console/BookLibrary.Console/Program.cs
+++ Issue4/BookLibrary.Console/BookLibrary.Console/Program.cs
@@ -1,16 +1,19 @@
-﻿namespace proj
+﻿using BookLibrary.Models;
+
+namespace proj
 {
+    
     public class User
     {
-        public string FirstName { get; set; } 
-        public string LastName { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
         public string Email { get; set; }
 
         public static List<User> Users = new List<User>();
         private static void validateMethod(User user)
         {
-           while (user.FirstName.Length < 3 || user.LastName.Length < 3 || user.Email.Length < 3 || user.Age < 10)
+            while (user.FirstName.Length < 3 || user.LastName.Length < 3 || user.Email.Length < 3 || user.Age < 10)
             {
                 Console.WriteLine("what's your name ? ");
                 user.FirstName = Console.ReadLine();
@@ -18,56 +21,57 @@
                 user.LastName = Console.ReadLine();
                 Console.WriteLine("what's your Email ? ");
                 user.Email = Console.ReadLine();
-              
-            }
-            }
 
-        private static void InitializeEmployees(List<User> users)
-        {
-            Users.Add(new User
-            {
-                Email = "tuba@gmail.com",
-                FirstName = "Tuba",
-                Age = 35,
-                LastName = "ojciechowski"
-            });
-
-            Users.Add(new User
-            {
-                Email = "atul@gmail.com",
-                FirstName = "Tul",
-                Age = 24,
-                LastName = "krol"
-            });
-            var querySyntax1 = from user in Users
-                               where user.FirstName.StartsWith("T")                 
-                               select user.FirstName;
-                               
-
-
-            Console.WriteLine("Where in querySyntax------");
-            foreach (var item in querySyntax1)
-            {
-                Console.WriteLine(item);
             }
         }
-        static void Main (string[] args)
+
+        //private static void InitializeEmployees(List<User> users)
+        //{
+        //    Users.Add(new User
+        //    {
+        //        Email = "tuba@gmail.com",
+        //        FirstName = "Tuba",
+        //        Age = 35,
+        //        LastName = "ojciechowski"
+        //    });
+
+        //    Users.Add(new User
+        //    {
+        //        Email = "atul@gmail.com",
+        //        FirstName = "Tul",
+        //        Age = 24,
+        //        LastName = "krol"
+        //    });
+        //    var querySyntax1 = from user in Users
+        //                       where user.FirstName.StartsWith("T")
+        //                       select user.FirstName;
+
+
+
+        //    Console.WriteLine("Where in querySyntax------");
+        //    foreach (var item in querySyntax1)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
+        static void Main(string[] args)
         {
             User user = new User();
-            //  Console.WriteLine("what's your age ? ");
-            // user.Age = int.Parse(Console.ReadLine());
-            //Console.WriteLine("what's your name ? ");
-            //user.FirstName = Console.ReadLine();
-            //Console.WriteLine("what's your Last name ? ");
-            //user.LastName = Console.ReadLine();
-            //Console.WriteLine("what's your Email ? ");
-            //user.Email = Console.ReadLine();
-           // validateMethod(user);
-            InitializeEmployees(Users);
-           // Console.WriteLine(user.FirstName + "  " + user.LastName+" " + user.Age +   "  " + user.Email);
+            Console.WriteLine("what's your age ? ");
+            user.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("what's your name ? ");
+            user.FirstName = Console.ReadLine();
+            Console.WriteLine("what's your Last name ? ");
+            user.LastName = Console.ReadLine();
+            Console.WriteLine("what's your Email ? ");
+            user.Email = Console.ReadLine();
+            validateMethod(user);
+            Class1 cl = new Class1();
+            //  InitializeEmployees(Users);
+            Console.WriteLine(user.FirstName + "  " + user.LastName + " " + user.Age + "  " + user.Email);
         }
 
-      
+
     }
 
 }
