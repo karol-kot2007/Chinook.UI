6600d3ef407669c43a86fcc606d562ad7e3a0ef9
karol <karol.kot.2007@gmail.com>
2024-10-10 10:12:20 +0200

    iu

---
+++ Issue6/Chinook.UI/Chinook.UI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Chinook.DAL.Models;
+namespace Chinook.UI
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      Artist artist = new Artist();
+      {
+        artist.Name = "Michael";
+        artist.ArtistId = 1;
+      }
+      this.DataContext = artist;
+    }
+
+    public void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+    class Artist
+    {
+      public Artist()
+      {
+
+      }
+      public string Name { get; set; }
+      public int ArtistId { get; set; }
+    }
+  }
+
+}
