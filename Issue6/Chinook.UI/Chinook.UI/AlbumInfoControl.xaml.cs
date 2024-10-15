68440bcb54740fe73a9bdc0e08fb037b6c99d63d
karol <karol.kot.2007@gmail.com>
2024-10-15 14:00:18 +0200

    wip

--- Issue6/Chinook.UI/Chinook.UI/AlbumInfoControl.xaml.cs
+++ Issue6/Chinook.UI/Chinook.UI/AlbumInfoControl.xaml.cs
@@ -1,27 +1,40 @@
-﻿using System.Windows;
+﻿using Chinook.DAL;
+using Chinook.DAL.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Chinook.UI
 {
-  
+
   public partial class AlbumInfoControl : UserControl
   {
+    public Mode DisplayMode { get; set; }
+    public ArtistContext ArtistId { get; private set; }
+
     public AlbumInfoControl()
     {
       InitializeComponent();
 
-    
+
     }
 
-  
+
 
     internal void Bind(AlbumInfoModel model, Mode mode)
     {
 
+
       GridAlbum.ItemsSource = model.Tracks;
       Button button = new Button();
-      button.Visibility = System.Windows.Visibility.Hidden;
+      // button.Visibility = System.Windows.Visibility.Hidden;
 
+      if (DisplayMode == Mode.View)
+      {
+        ArtistName.IsReadOnly = true;
+        AlbumName.IsReadOnly = true;
+      }
+     
     }
 
     private void dgUsers_AddingNewItem(object sender, AddingNewItemEventArgs e)
@@ -35,7 +48,7 @@ namespace Chinook.UI
       System.Environment.Exit(0);
     }
 
-    private void ok_Button(object sender, RoutedEventArgs e)
+    private void ok_Button(object sender, RoutedEventArgs e, ArtistContext ArtistId)
     {
 
     }
--- Issue6/Chinook.UI/Chinook.UI/AlbumInfoWindow.xaml.cs
+++ Issue6/Chinook.UI/Chinook.UI/AlbumInfoWindow.xaml.cs
@@ -1,46 +1,56 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Chinook.DAL;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Windows;
 
 namespace Chinook.UI
 {
-  
   public partial class AlbumInfoWindow : Window
   {
+    public AlbumInfoModel AlbumInfoModel { get; set; }
     public Mode DisplayMode { get; set; }
-
     public AlbumInfoWindow()
     {
       InitializeComponent();
-
     }
     protected override void OnInitialized(EventArgs e)
     {
       base.OnInitialized(e);
     }
-
     internal void ShowWithData(AlbumInfoModel model)
     {
       DataContext = model;
-      AlbumInfoControl.Bind(model,DisplayMode);
-      if(DisplayMode==Mode.View)
+      AlbumInfoControl.Bind(model, DisplayMode);
+      if (DisplayMode == Mode.View)
       {
         CancelBtn.Visibility = Visibility.Collapsed;
+
       }
+      if (DisplayMode == Mode.Edit)
+      {
 
-
-
+        CloseBtn.Visibility = Visibility.Collapsed; 
+      }
+      AlbumInfoModel = model;
       ShowDialog();
     }
 
     private void CloseBtn_Click(object sender, RoutedEventArgs e)
     {
-      //zrobic visibilty w zalesznosci od 
+      
       Close();
     }
     private void OkBtn_Click(object sender, RoutedEventArgs e)
     {
       //zrobic visibilty w zalesznosci od 
-      
+      var artistContext = new ArtistContext();
+
+      var artist = artistContext.Artists.Where(i => i.ArtistId == AlbumInfoModel.ArtistId).Single();
+      artist.Name = AlbumInfoModel.ArtistName;
+      artistContext.SaveChanges();
+
+      Close();
+      //zrobic visibilty w zalesznosci od 
+
     }
     private void CancelBtn_Click(object sender, RoutedEventArgs e)
     {
@@ -48,6 +58,5 @@ namespace Chinook.UI
 
     }
     //dodac  private void
-
   }
 }
